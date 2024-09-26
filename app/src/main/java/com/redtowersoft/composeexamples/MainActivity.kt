654a714f88339171674046801a1250e656ea122d
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-09-26 06:49:32 -0300

    C2 Finished

--- app/src/main/java/com/redtowersoft/composeexamples/MainActivity.kt
+++ app/src/main/java/com/redtowersoft/composeexamples/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,9 +27,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-            DummyComposableFunction(
+            DummyComposableListFunction(
                         dummyName = "Rufo",
-                        modifier = Modifier.fillMaxWidth().padding(16.dp).background(Color.Red)
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .background(Color.Red)
                     )
                 }
             }
@@ -34,8 +43,53 @@ fun DummyComposableFunction(dummyName: String, modifier: Modifier = Modifier) {
     Text(modifier = modifier, text = "Dummy name example: $dummyName")
 }
 
+@Composable
+fun DummyComposableColumnFunction(dummyName1: String, dummyName2: String,modifier: Modifier = Modifier) {
+    Column  {
+        Text(modifier = modifier, text = "Dummy name 1 example: $dummyName1")
+        Text(modifier = modifier, text = "Dummy name 2 example: $dummyName2")
+    }
+}
+
+@Composable
+fun DummyComposableListFunction(dummyName: String, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
+        items(100) {
+            Text(
+                modifier = modifier,
+                text = "$dummyName $it"
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DummyComposableFunctionPreview() {
-    DummyComposableFunction("Rufo",modifier = Modifier.fillMaxWidth().padding(5.dp).background(Color.Red))
+    DummyComposableFunction("Rufo",modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp)
+        .background(Color.Red))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DummyComposableColumnFunctionPreview() {
+    DummyComposableColumnFunction("Rufo", "Rufa", modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp)
+        .background(Color.Red))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DummyComposableListFunctionPreview() {
+    DummyComposableListFunction("Rufo", modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp)
+        .background(Color.Red))
 }
